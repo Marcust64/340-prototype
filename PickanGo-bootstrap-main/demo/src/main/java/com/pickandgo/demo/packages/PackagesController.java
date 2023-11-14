765094e0a5f5aba4d86501126498244648b2077a
Kenneth Alvarado <kennethalvarado70@gmail.com>
2023-11-13 19:35:33 -0500

    can create package in library.html

--- PickanGo-bootstrap-main/demo/src/main/java/com/pickandgo/demo/packages/PackagesController.java
+++ PickanGo-bootstrap-main/demo/src/main/java/com/pickandgo/demo/packages/PackagesController.java
@@ -2,11 +2,19 @@ package com.pickandgo.demo.packages;
 
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.pickandgo.demo.packages.Packages;
+import com.pickandgo.demo.packages.PackagesService;
+
+import java.util.List;
 
 
 /**
@@ -14,67 +22,37 @@ import org.springframework.web.bind.annotation.PostMapping;
  * @author Marcus Thompson
  */
 @Controller
+@RequestMapping("/api/packages")
 public class PackagesController {
     
     @Autowired
     PackagesService packageService;
  
-        @GetMapping("/user/library-user")
-        public String getAllPackages(Model model) {
-        model.addAttribute("packageList",
-                packageService.getAllPackages());
-        return "user/library-user";
-    }
-/*
-    @GetMapping("/search")
-    public String getPackage(Model model, @Param("keyword") String keyword) {
-        model.addAttribute("packageList",
-                packageService.getAllPackages(keyword));
-        model.addAttribute("keyword", keyword);
-        return "user/library-user";
-    }
-*/
-    @GetMapping("/user/id={packageId}")
-    public String getPackage(@PathVariable long packageId, Model model) {
-        model.addAttribute("product",
-                packageService.getPackage(packageId));
-        return "product/views-user";
-    }
-/*
-    @GetMapping("/user/delete/id={packageId}")
-    public String deletePackage(@PathVariable long packageId, Model model) {
-        packageService.deletePackage(packageId);
-        return "redirect:user/library-user";
-    }
-*/
-    @GetMapping("/user/create-user")
-    public String showCreateForm(){
-        return "user/create-user";
-    }
-    
-    @PostMapping("/package/create")
-    public String createPackage(Packages packages) {
-        
-        packageService.savePackage(packages);
-        return "redirect:/user/library-user";
-    }
-/*
-    @PostMapping("/update")
-    public String upateProduct(Packages packages) {
-        packageService.savePackage(packages);
-        return "redirect:/product/all";
+   
+
+    // Method to handle the creation of a new package
+    @PostMapping
+    public ResponseEntity<Packages> createPackage(@RequestBody Packages packages) {
+        try {
+            Packages newPackage = packageService.savePackage(packages);
+            return new ResponseEntity<>(newPackage, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @GetMapping("/new-product")
-    public String newProductForm(Model model) {
-        return "product/new-product";
-    }
-*/
-    @GetMapping("/update/id={packageId}")
-    public String updateProductForm(@PathVariable long packageId, Model model) {
-       // model.addAttribute("product",
-                //packageService.getPackage(packageId));
-        return "user/views-user";
+    // Method to retrieve all packages
+    @GetMapping
+    public ResponseEntity<List<Packages>> getAllPackages() {
+        try {
+            List<Packages> packages = packageService.getAllPackages();
+            if (packages.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(packages, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
      
 }
--- PickanGo-bootstrap-main/demo/src/main/java/com/pickandgo/demo/packages/PackagesService.java
+++ PickanGo-bootstrap-main/demo/src/main/java/com/pickandgo/demo/packages/PackagesService.java
@@ -1,65 +1,43 @@
 package com.pickandgo.demo.packages;
 
-import java.util.List;
+import com.pickandgo.demo.packages.Packages;
+import com.pickandgo.demo.packages.PackagesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- *
- * @author Marcus Thompson
- */
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class PackagesService {
+
     @Autowired
-    private PackagesRepository repo;
+    private PackagesRepository packagesRepository;
 
-    /**
-     * Get all products.
-     *
-     * @return the list of products.
-     */
+    // Save a new package
+    public Packages savePackage(Packages packages) {
+        return packagesRepository.save(packages);
+    }
+
+    // Get all packages
     public List<Packages> getAllPackages() {
-        return repo.findAll();
+        return packagesRepository.findAll();
     }
 
-    /**
-     * Get all products that match the keyword.
-     *
-     * @param keyword the search term.
-     * @return the list of products.
-     */
-  /*  public List<Packages> getAllPackages(String keyword) {
-        if (keyword != null) {
-            return repo.search(keyword);
-        }
-        return repo.findAll();
-    }
-*/
-    /**
-     * Get a single product by ID
-     *
-     * @param packageId
-     * @return the product
-     */
-    public Packages getPackage(long packageId) {
-        return repo.getReferenceById(packageId);
+    // Get a package by ID
+    public Optional<Packages> getPackageById(Long id) {
+        return packagesRepository.findById(id);
     }
 
-    /**
-     * Delete a single product by ID
-     *
-     * @param packageId
-     */
-    public void deletePackage(long packageId) {
-        repo.deleteById(packageId);
+    // Update a package
+    public Packages updatePackage(Packages packages) {
+        return packagesRepository.save(packages);
     }
 
-    /**
-     * Save a product entry.
-     *
-     * @param packages
-     */
-    void savePackage(Packages packages) {
-        repo.save(packages);
+    // Delete a package
+    public void deletePackage(Long id) {
+        packagesRepository.deleteById(id);
     }
+
+    // Additional methods as per your application's requirement
 }
