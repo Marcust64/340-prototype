18d150ad121b2ace103b73ad9d16c83c0a144d59
Kenneth Alvarado <kennethalvarado70@gmail.com>
2023-12-05 01:22:16 -0500

    Added API to VIEW/EDIT page in library for Tour

--- PickanGo-bootstrap-main/demo/src/main/java/com/pickandgo/demo/Application.java
+++ PickanGo-bootstrap-main/demo/src/main/java/com/pickandgo/demo/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.pickandgo.demo.packages, com.pickandgo.demo.user, com.pickandgo.demo.Security, com.pickandgo.demo.PackagesTour")
+@ComponentScan("com.pickandgo.demo.packages, com.pickandgo.demo.user, com.pickandgo.demo.Security, com.pickandgo.demo.PackagesTour, com.pickandgo.demo.Applications")
 public class Application {
 
 	public static void main(String[] args) {
